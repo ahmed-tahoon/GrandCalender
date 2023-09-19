39e184100f86573689ae8f17aafc80f4b0c4122d
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-09-19 20:49:05 +0300

    calen

--- src/components/layout/TopLoader.jsx
+++
@@ -1,6 +0,0 @@
-export default function TopLoader() {
-
-    return (
-        <div className="grandizer-top-loader"></div>
-    );
-}
--- src/utils/navigation.js
+++
@@ -1,8 +0,0 @@
-const navigation = [
-    { name: 'Dashboard', to: '/dashboard' },
-    { name: 'Calendars', to: '/dashboard/calendars' },
-    { name: 'Bookings', to: '/dashboard/bookings' },
-    { name: 'Availability', to: '/dashboard/availability' },
-];
-
-export default navigation;
